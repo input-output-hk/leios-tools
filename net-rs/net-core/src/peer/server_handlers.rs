21af21a83d8f93f34e28df385a197164723ae4f1
Dmitry Shtukenberg <dmitry.shtukenberg@gmail.com>
2026-08-13 20:23:31 +0300

    Fixed after preparing tests

--- net-rs/net-core/src/peer/server_handlers.rs
+++ net-rs/net-core/src/peer/server_handlers.rs
@@ -2292,7 +2292,7 @@ mod tests {
 
         struct StubResolver(std::collections::HashMap<TxId, TxBody>);
         impl TxBodyResolver for StubResolver {
-            fn resolve_body(&self, tx_id: &TxId) -> Option<TxBody> {
+            fn resolve_body(&self, _slot: u64, tx_id: &TxId) -> Option<TxBody> {
                 self.0.get(tx_id).cloned()
             }
         }
